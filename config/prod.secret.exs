7099540467b782df6c31c5205f4d945f1058c049
Arthur Colle <hello@arthurcolle.com>
2015-04-27 09:29:21 -0400

    Tutorial from 4/25/2015

---
+++ config/prod.secret.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :phoenix_exrethinkdb_chat, PhoenixExrethinkdbChat.Endpoint,
+  secret_key_base: "Mogz+25LLGizgfjECLzjcrl1U//49gR56tx8aQudE1xser22oNbwMR6tTZgUYW6y"
